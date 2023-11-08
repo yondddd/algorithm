73aff0023e3ed3745670129d6912f3e0886e86dd
wangjielong <wangjielong>
2023-11-08 22:45:30 +0800

    first

---
+++ src/main/java/org/tom/firstWeek/courseIntrodution/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.tom.firstWeek.courseIntrodution.hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
